87499bb8ac400848dd3ad15331d61e9314c86573
Martin Enriquez <martinantraxelm1@gmail.com>
2023-10-24 13:57:03 -0700

    finished working on simple functions

---
+++ simple_functions.js
@@ -0,0 +1,23 @@
+function favoriteThings() {
+    console.log("Martin Enriquez");
+    console.log("Shrek");
+    console.log("Solo Artist Melendi");
+
+}
+
+favoriteThings();
+
+function whyImhere() {
+    console.log("I joined YearUp to learn how to become a web developer and to get a good " + 
+    "job that can support my wildest dreams. ");
+    
+}
+
+whyImhere();
+
+function favoritePlace() {
+    console.log("My favorite place to visit in the world is Madrid. " + 
+    "I love to visit Madrid becuase I like the architecture, it's pleasant to me. ")
+}
+
+favoritePlace();
