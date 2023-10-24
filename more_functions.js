17deab82c81851bc8bd27e3a44f919065c6f2c08
Martin Enriquez <martinantraxelm1@gmail.com>
2023-10-24 14:40:21 -0700

    finished displayMailingLabel

---
+++ more_functions.js
@@ -0,0 +1,8 @@
+
+function displayMailingLabel(firstName, address, city, state, zip) {
+   console.log(firstName);
+   console.log(address);
+   console.log(city + state + zip);
+}
+
+let mailingLabel = displayMailingLabel("Martin ", "123 Main St. ", "Phoenix ", "Arizona ", "85008");
